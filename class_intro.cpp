c883958a807e8ba8237820aca62f007b8ed53899
murlidharpathakprogrammer <murlidharpathakprogrammer@gmail.com>
2021-06-23 23:45:35 +0530

    Add class intro cpp file

--- class_intro.cpp
+++ class_intro.cpp
@@ -3,13 +3,27 @@ using namespace std;
 
     class BankAccount{
         public:
-            void sayHi(){
-                cout<<"Hi"<<endl;
+        string name;
+        long int accountNumber;
+        int balance;
+        void showDetails(){
+                cout<<"Account Number: "<<accountNumber<<endl;
+                cout<<"Customer Name: "<<name<<endl;
+                cout<<"Balance: "<<balance<<endl;
             }
     };
 
 int main(){
-    BankAccount test;
-    test.sayHi();
+    BankAccount one;
+    one.name = "Murlidhar Pathak";
+    one.accountNumber= 857496524163;
+    one.balance=50000000;
+    one.showDetails();
+    cout<<"\n";
+    BankAccount two;
+    two.name = "Gajodhar Pandey";
+    two.accountNumber= 748596415263;
+    two.balance=7556000;
+    two.showDetails();
     return 0;
 }
